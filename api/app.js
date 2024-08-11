bc6303312e7cb3c33f445d045db968cc445e0c5c
Anshu <95614796+anshu11n@users.noreply.github.com>
2024-08-11 22:29:01 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -14,7 +14,7 @@ import bookRoute from "./routes/book.route.js"
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true, methods: 'GET,HEAD,PATCH,PUT,POST,DELETE', allowedHeaders: 'Content-Type,Authorization' }));
 app.use(express.json());
 app.use(cookieParser());
 
